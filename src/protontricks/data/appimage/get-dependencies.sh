4e16870f427f4f953984fd9f797d450ec28250ed
Fatih Bakal <fatihbakal7@protonmail.com>
2026-06-26 16:51:21 +0300

    push

---
+++ src/protontricks/data/appimage/get-dependencies.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -eu
+
+ARCH=$(uname -m)
+
+echo "Installing package dependencies..."
+echo "---------------------------------------------------------------"
+pacman -Syu --noconfirm protontricks zenity yad
+
+echo "Installing debloated packages..."
+echo "---------------------------------------------------------------"
+./get-debloated-pkgs.sh --add-common --prefer-nano
+
+# Comment this out if you need an AUR package
+#make-aur-package PACKAGENAME
+
+# If the application needs to be manually built that has to be done down here
