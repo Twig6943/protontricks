0e14d41a63c61de9adb78e3cb9396dec230b2a52
Fatih Bakal <fatihbakal7@protonmail.com>
2026-06-26 18:13:44 +0300

    works

--- src/protontricks/data/appimage/get-dependencies.sh
+++ src/protontricks/data/appimage/get-dependencies.sh
@@ -1,8 +1,14 @@
+
 #!/bin/sh
 
 set -eu
 
 ARCH=$(uname -m)
+
+DEBLOATED_PKGS="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+wget --retry-connrefused --tries=30 "$DEBLOATED_PKGS" -O ./get-debloated-pkgs
+
+chmod +x ./quick-sharun ./get-debloated-pkgs
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
@@ -10,7 +16,7 @@ pacman -Syu --noconfirm protontricks zenity yad
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-./get-debloated-pkgs.sh --add-common --prefer-nano
+./get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
 #make-aur-package PACKAGENAME
--- src/protontricks/data/appimage/make-appimage.sh
+++ src/protontricks/data/appimage/make-appimage.sh
@@ -6,12 +6,10 @@ ARCH=$(uname -m)
 VERSION=$(pacman -Q protontricks | awk '{print $2; exit}') # example command to get version of application here
 
 SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
-DEBLOATED_PKGS="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
 
 # ADD LIBRARIES
-wget --retry-connrefused --tries=30 "$DEBLOATED_PKGS" -O ./get-debloated-pkgs
 wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
-chmod +x ./quick-sharun ./get-debloated-pkgs
+chmod +x ./quick-sharun
 
 export ARCH VERSION
 #export APPDIR="$PWD/AppDir"
@@ -26,8 +24,26 @@ export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 
 # Deploy dependencies
-./quick-sharun /usr/bin/*tricks* /usr/bin/zenity /usr/bin/yad
+quick-sharun \
+	/usr/bin/*tricks*        \
+	/usr/lib/libopenjp2.so*  \
+	/usr/lib/libtiff.so*     \
+	/usr/lib/libimagequant.so* \
+	/usr/bin/zenity \
+	/usr/bin/yad
+
 echo 'unset VK_DRIVER_FILES' >> ./AppDir/.env
+# Fix python symlinks to point to 3.11 which has the stdlib bundled
+rm -f ./AppDir/shared/bin/python ./AppDir/shared/bin/python3
+ln -s python3.11 ./AppDir/shared/bin/python
+ln -s python3.11 ./AppDir/shared/bin/python3
+
+# Install protontricks and its Python dependencies into the AppDir's Python 3.11
+SITE_PACKAGES="$PWD/AppDir/lib/python3.11/site-packages"
+python3.11 -m venv /tmp/protontricks-venv
+/tmp/protontricks-venv/bin/pip install --target="$SITE_PACKAGES" --no-deps vdf Pillow
+rsync -a --exclude='data/appimage' "$PWD/../../" "$SITE_PACKAGES/protontricks/"
+rm -rf /tmp/protontricks-venv
 
 cc -shared -fPIC -O2 -o ./AppDir/lib/execve-sharun-hack.so execve-sharun-hack.c -ldl
 echo 'execve-sharun-hack.so' >> ./AppDir/.preload
